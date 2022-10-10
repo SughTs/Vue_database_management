6020f5db617a62a1e6685de785cca9df61c269fb
Sught <Liu220697368@outlook.com>
2022-10-10 21:34:44 +0800

    Ver 0.10

---
+++ src/views/recommend.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="recommend">
+    <h1>请输入对象ID</h1>
+    <!--此处插入ID搜索与数据库交互，在做了在做了！-->
+    <h1>根据对象的阅读兴趣，推荐以下书籍</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "recommend"
+}
+</script>
+<style scoped>
+</style>
